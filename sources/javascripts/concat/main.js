af77c6858f2b21dccacc768df9a96146cef7dce0
PeeterPaal <peeterpaal@gmail.com>
2023-11-29 10:11:32 +0200

    Add multi-assets support to auto-gen pr. page

--- sources/javascripts/concat/main.js
+++ sources/javascripts/concat/main.js
@@ -1214,18 +1214,6 @@ $.fn[ pluginName ] = function ( arg ) {
   };
 
   // ===========================================================================
-  // Opens product admin view on product image click
-  // ===========================================================================
-
-  var handleProductImageClick = function(product_id) {
-    if (editmode()) {
-      $('.product-content .product-image').click(function() {
-        window.open('/admin/ecommerce/products/' + product_id, '_blank').focus();
-      });
-    }
-  };
-
-  // ===========================================================================
   // Function to detect if site is displayed in editmode.
   // ===========================================================================
   var editmode = function () {
@@ -1254,8 +1242,7 @@ $.fn[ pluginName ] = function ( arg ) {
     bindRootItemSettings: bindRootItemSettings,
     bindContentItemImgDropAreas: bindContentItemImgDropAreas,
     bindContentItemImageCropToggle: bindContentItemImageCropToggle,
-    handleProductPageContent: handleProductPageContent,
-    handleProductImageClick: handleProductImageClick
+    handleProductPageContent: handleProductPageContent
   });
 
   // ===========================================================================
